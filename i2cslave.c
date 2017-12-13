7b2805075d3c0ea6c8ef2e4b06e51bd6a9b42109
marilafo <mar.lafon.ml@gmail.com>
2017-12-13 17:39:47 +0000

    add slave change

--- i2cslave.c
+++ i2cslave.c
@@ -12,7 +12,6 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <linux/module.h>
 
 #define TX_BUF_SIZE   4000
 
@@ -59,26 +58,8 @@ int rmmod_module(char *name){
 
 int main(int argc, char **argv)
 {
-
-	if(insmod_module("bcm2835_slave_mod.ko")!=0){
-		perror("error insmod");
-		return EXIT_FAILURE;	
-	}
-	printf("Insmod done");
-
-	/*if(rmmod_module("bcm2835_slave_mod")!=0){
-		perror("error rmmod");
-		return EXIT_FAILURE;
-	}
-	
-	printf("rmmod done");
-	*/
-	
-	return EXIT_SUCCESS;
-
-
-
-/*	char tx_buffer[TX_BUF_SIZE];
+	char tx_buffer[TX_BUF_SIZE];
+	char ans_buffer[TX_BUF_SIZE];
 	int fd;
 	uint8_t data;
 	int length;
@@ -89,16 +70,67 @@ int main(int argc, char **argv)
 	FILE *usage_file = stderr;
 	const char *input = DEFAULT_DEVICE;
 
-	if (optind < argc) {
-		input = argv[optind];
-	}
 
-	if ((fd = open(input, O_RDWR)) == -1) {
-		perror("open i2c device");
-		exit(EXIT_FAILURE);
-	}
 
+	
+	int position = 1;
 	int random = 0;
+	int i = 0;
+	int has_addr = 0;
+	char addr;
+	while(has_addr != 1){
+	
+		//Insertion du module aprés un temps aléatoire
+		srand(time(NULL));
+		random=rand()%1000000;
+		usleep(random+position);
+
+		if(insmod_module("bcm2835_slave_mod.ko")!=0){
+			perror("error insmod");
+			return EXIT_FAILURE;	
+		}
+
+		//Ouverture du fichier du module
+		if ((fd = open(input, O_RDWR)) == -1) {
+			perror("open i2c device");
+			exit(EXIT_FAILURE);
+		}
+
+
+		//Tentative de récupération de l'adresse :
+		for (i = 0; i < 3 ; i++){
+			length = read(fd, tx_buffer, TX_BUF_SIZE);
+			if (i > 0 && tx_buffer[0] == position)
+				has_addr = 1;		
+
+			for(i = 0; i < length; i++)
+			{
+				printf("Data received : %d \n", tx_buffer[i]);
+			}
+
+			ans_buffer[0]=position;
+
+			write(fd, tx_buffer, length);
+		}
+		//Aprés 2 essais on s'enlève et on réétablie la connexion
+		if (i == 3){
+			close(fd)
+			if(rmmod_module("bcm2835_slave_mod")!=0){
+				perror("error rmmod");
+				return EXIT_FAILURE;
+			}
+				
+		}
+		else{
+			addr = tx_buffer[1];
+			break;
+		}
+	}
+
+	//Changement d'adresse 
+	
+
+	//Attente des demande du slave, peut etre faire un temps d'attente, ou une sortie
 	while (1) {	
 			length = read(fd, tx_buffer, TX_BUF_SIZE);
 			for(i = 0; i < length; i++)
@@ -119,7 +151,13 @@ int main(int argc, char **argv)
 			write(fd, tx_buffer, length);
 	}
 
+	//Suppression du module
 	close(fd);
-	return 0;*/
+	if(rmmod_module("bcm2835_slave_mod")!=0){
+		perror("error rmmod");
+		return EXIT_FAILURE;
+	}
+	
+	return 0;
 	
 }
